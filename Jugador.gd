b443994d9dac01f55755422e9af0f2545fcd4cc9
Nicolas <lasso.nicolas@correounivalle.edu.co>
2020-11-06 15:08:53 -0500

    Corregido bug del movimiento del jugador

--- Jugador.gd
+++ Jugador.gd
@@ -12,23 +12,28 @@ func _physics_process(delta):
 	else: 
 		apply_movement(axis* ACCEL *delta)
 	motion = move_and_slide(motion)
-	
-	
+
+
 func get_input_axis():
 	var axis = Vector2.ZERO
 	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	return axis.normalized()
-	
+
 func apply_friction(amount):
 	if motion.length() > amount:
 		motion -= motion.normalized() * amount  
 	else:
 		motion = Vector2.ZERO
-		
+
 func apply_movement(acceleration):
 	motion += acceleration
 	motion = motion.clamped(MAX_SPEED)
+
+
+
+
+
 
 
 func _on_Area2D_area_entered(area):
