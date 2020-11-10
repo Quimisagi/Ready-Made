86918f4625cd56cff8dc2e917dd4785695bcc1e1
Juan Velandia <juankz@protonmail.com>
2020-11-09 21:51:11 -0500

    Corregir recuerdo especial en la escena 3

--- Escenas/RecuerdoEspecial.gd
+++ Escenas/RecuerdoEspecial.gd
@@ -1,16 +1,13 @@
-extends Sprite
+extends Node2D
 
+var obtained = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_PlayerDetector_body_entered(body):
+	if body.is_in_group('player') && !obtained:
+		obtained = true
+		body.collected_memories += 1
+		body._check_if_memories_completed()
+		$esfera/AnimationPlayer.play("disappear")
+		$AudioStreamPlayer2D.play()
+		yield(get_tree().create_timer(0.5), "timeout")
+		queue_free()
